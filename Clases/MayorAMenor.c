bfb996b6980873acd3818f0d00222e8f08179397
Servando Matias <servandokratos@gmail.com>
2018-08-24 23:45:53 -0500

    funcion ordenamiento completa
    se ordenan 3 numeros de mayor a menor

--- Clases/MayorAMenor.c
+++ Clases/MayorAMenor.c
@@ -2,6 +2,42 @@
 
 int main()
 {
-	printf("hola mundo\n");
+	int a;
+	int b;
+	int c;
+	int aux;
+
+	printf("introduce el primer numero\n");
+	scanf("%d", &a);
+	printf("introduce el segundo numero\n");
+	scanf("%d", &b);
+	printf("introduce eltercer numero\n");
+	scanf("%d", &c);
+
+	if (b > a)
+	{
+		aux=a;
+		a = b;
+		b = aux;
+	}
+
+	if (c > a)
+	{
+		aux = a;
+		a = c;
+		c = aux;
+	}
+
+
+	if (c > b)
+	{
+		aux = b;
+		b = c;
+		c = aux;
+	}
+
+	printf("Numero mayor = %d\n", a);
+	printf("Numero medio = %d\n", b);
+	printf("Numero menor = %d\n", c);
 	return 0;
 }
